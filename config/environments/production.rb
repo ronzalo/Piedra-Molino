c0103a0693ec53c0c3218143d9a3e3a6ca965d2c
Gonzalo Moreno <gonzalo@gonzalo-Satellite-L515.(none)>
2012-06-03 15:42:31 -0400

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-ActiveInvoices::Application.configure do
+Project::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
