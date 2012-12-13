c1fc8d21365377c045ce15a4a7dbf447513aee01
Gonzalo <gmoreno@acid.cl>
2012-12-13 16:04:56 -0300

    proce

--- app/models/sales_invoice.rb
+++ app/models/sales_invoice.rb
@@ -5,19 +5,12 @@ class SalesInvoice < ActiveRecord::Base
   attr_accessible :cliente_id, :total_venta, :folio, :fecha_emision, :neto, :det_documento_attributes
   accepts_nested_attributes_for :det_documento
 
-  after_save :set_neto
+  ##after_save :set_neto
   
   def folio
     self.id
   end
   
-  def set_neto
-    id_f = self.id
-    sql = ActiveRecord::Base.connection();
-    sql.execute "call set_neto_sale_i(#{id_f})";
-    sql.begin_db_transaction
-  end
-
   def this_month
     where('fecha_emision >= ?', Date.new(Time.now.year, Time.now.month, 1).to_datetime)
   end
